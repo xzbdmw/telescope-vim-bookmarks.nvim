896b4afc8d8e991af4dff33fbc7f5b3176177dc2
xzbdmw <97848247+xzbdmw@users.noreply.github.com>
2024-02-20 12:53:14 +0800

    Update vim_bookmarks.lua

--- lua/telescope/_extensions/vim_bookmarks.lua
+++ lua/telescope/_extensions/vim_bookmarks.lua
@@ -1,22 +1,22 @@
-local finders = require('telescope.finders')
-local pickers = require('telescope.pickers')
-local entry_display = require('telescope.pickers.entry_display')
-local conf = require('telescope.config').values
-local make_entry = require('telescope.make_entry')
+local finders = require("telescope.finders")
+local pickers = require("telescope.pickers")
+local entry_display = require("telescope.pickers.entry_display")
+local conf = require("telescope.config").values
+local make_entry = require("telescope.make_entry")
 
-local utils = require('telescope.utils')
+local utils = require("telescope.utils")
 
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
-local bookmark_actions = require('telescope._extensions.vim_bookmarks.actions')
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local bookmark_actions = require("telescope._extensions.vim_bookmarks.actions")
 
 local function get_bookmarks(files, opts)
     opts = opts or {}
     local bookmarks = {}
 
-    for _,file in ipairs(files) do
-        for _,line in ipairs(vim.fn['bm#all_lines'](file)) do
-            local bookmark = vim.fn['bm#get_bookmark_by_line'](file, line)
+    for _, file in ipairs(files) do
+        for _, line in ipairs(vim.fn["bm#all_lines"](file)) do
+            local bookmark = vim.fn["bm#get_bookmark_by_line"](file, line)
 
             local text = bookmark.annotation ~= "" and "Annotation: " .. bookmark.annotation or bookmark.content
             if text == "" then
@@ -29,7 +29,7 @@ local function get_bookmarks(files, opts)
                 table.insert(bookmarks, {
                     filename = file,
                     lnum = tonumber(line),
-                    col=1,
+                    col = 1,
                     text = text,
                     sign_idx = bookmark.sign_idx,
                 })
@@ -44,14 +44,14 @@ local function make_entry_from_bookmarks(opts)
     opts = opts or {}
     opts.tail_path = vim.F.if_nil(opts.tail_path, true)
 
-    local displayer = entry_display.create {
-        separator = "▏",
+    local displayer = entry_display.create({
+        separator = " ",
         items = {
-            { width = opts.width_line or 5 },
-            { width = opts.width_text or 60 },
-            { remaining = true }
-        }
-    }
+            { width = 10 },
+            { width = 30 },
+            { remaining = true },
+        },
+    })
 
     local make_display = function(entry)
         local filename
@@ -64,13 +64,13 @@ local function make_entry_from_bookmarks(opts)
             end
         end
 
-        local line_info = {entry.lnum, "TelescopeResultsLineNr"}
+        local line_info = { "->" .. entry.lnum, "TelescopeResultsLineNr" }
 
-        return displayer {
+        return displayer({
+            { filename, "Comment" },
             line_info,
             entry.text:gsub(".* | ", ""),
-            filename,
-        }
+        })
     end
 
     return function(entry)
@@ -78,10 +78,7 @@ local function make_entry_from_bookmarks(opts)
             valid = true,
 
             value = entry,
-            ordinal = (
-            not opts.ignore_filename and filename
-            or ''
-            ) .. ' ' .. entry.text,
+            ordinal = (not opts.ignore_filename and filename or "") .. " " .. entry.text,
             display = make_display,
 
             filename = entry.filename,
@@ -98,64 +95,68 @@ local function make_bookmark_picker(filenames, opts)
     local make_finder = function()
         local bookmarks = get_bookmarks(filenames, opts)
 
-        if vim.tbl_isempty(bookmarks) then 
+        if vim.tbl_isempty(bookmarks) then
             print("No bookmarks!")
             return
         end
 
-        return finders.new_table {
+        return finders.new_table({
             results = bookmarks,
             entry_maker = make_entry_from_bookmarks(opts),
-        }
+        })
     end
-    
+
     local initial_finder = make_finder()
-    if not initial_finder then return end
+    if not initial_finder then
+        return
+    end
 
-    pickers.new(opts, {
-        prompt_title = opts.prompt_title or "vim-bookmarks",
-        finder = initial_finder,
-        previewer = conf.qflist_previewer(opts),
-        sorter = conf.generic_sorter(opts),
+    pickers
+        .new(opts, {
+            prompt_title = opts.prompt_title or "vim-bookmarks",
+            finder = initial_finder,
+            previewer = conf.qflist_previewer(opts),
+            sorter = conf.generic_sorter(opts),
 
-        attach_mappings = function(prompt_bufnr, map) 
-            local refresh_picker = function() 
-                local new_finder = make_finder()
-                if new_finder then
-                    action_state.get_current_picker(prompt_bufnr):refresh(make_finder())
-                else
-                    actions.close(prompt_bufnr)
+            attach_mappings = function(prompt_bufnr, map)
+                local refresh_picker = function()
+                    local new_finder = make_finder()
+                    if new_finder then
+                        action_state.get_current_picker(prompt_bufnr):refresh(make_finder())
+                    else
+                        actions.close(prompt_bufnr)
+                    end
                 end
-            end
 
-            bookmark_actions.delete_selected:enhance { post = refresh_picker }
-            bookmark_actions.delete_at_cursor:enhance { post = refresh_picker }
-            bookmark_actions.delete_all:enhance { post = refresh_picker }
-            bookmark_actions.delete_selected_or_at_cursor:enhance { post = refresh_picker }
+                bookmark_actions.delete_selected:enhance({ post = refresh_picker })
+                bookmark_actions.delete_at_cursor:enhance({ post = refresh_picker })
+                bookmark_actions.delete_all:enhance({ post = refresh_picker })
+                bookmark_actions.delete_selected_or_at_cursor:enhance({ post = refresh_picker })
 
-            return true
-        end
-    }):find()
+                return true
+            end,
+        })
+        :find()
 end
 
 local all = function(opts)
-    make_bookmark_picker(vim.fn['bm#all_files'](), opts)
+    make_bookmark_picker(vim.fn["bm#all_files"](), opts)
 end
 
 local current_file = function(opts)
     opts = opts or {}
-    opts = vim.tbl_extend('keep', opts, {path_display = true})
+    opts = vim.tbl_extend("keep", opts, { path_display = true })
 
-    make_bookmark_picker({vim.fn.expand('%:p')}, opts)
+    make_bookmark_picker({ vim.fn.expand("%:p") }, opts)
 end
 
-return require('telescope').register_extension {
+return require("telescope").register_extension({
     exports = {
         -- Default when to argument is given, i.e. :Telescope vim_bookmarks
         vim_bookmarks = all,
 
         all = all,
         current_file = current_file,
-        actions = bookmark_actions
-    }
-}
+        actions = bookmark_actions,
+    },
+})
